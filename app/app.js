ba1afa637b1622ad8bcea04ac40cd580415118fc
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-10-30 16:33:57 -0400

    added functionality to edit page

--- app/app.js
+++ app/app.js
@@ -50,7 +50,41 @@ function createListener() {
   });
 }
 
+function editListener() {
+  $("#edit").click(function (e) {
+    e.preventDefault();
+
+    let userObj = {
+      img: "none",
+      name: "Deadlift",
+      description: "barbell deadlift",
+      time: "10 minutes",
+      cal: "370cal",
+      setOne: "275x5",
+      setTwo: "295x5",
+      setThree: "315x5",
+      instOne: "set shins close to bar",
+      instTwo: "pull slack out of bar",
+      instThree: "pick up bar",
+    };
+
+    console.log(userObj);
+    $("#img").val(userObj.img);
+    $("#name").val(userObj.name);
+    $("#description").val(userObj.description);
+    $("#time").val(userObj.time);
+    $("#cal").val(userObj.cal);
+    $("#setOne").val(userObj.setOne);
+    $("#setTwo").val(userObj.setTwo);
+    $("#setThree").val(userObj.setThree);
+    $("#instOne").val(userObj.instOne);
+    $("#instTwo").val(userObj.instTwo);
+    $("#instThree").val(userObj.instThree);
+  });
+}
+
 $(document).ready(function () {
   navListeners();
   createListener();
+  editListener();
 });
