111d5dbe097e8a8626552fb000031094bffe174c
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-10-30 16:09:01 -0400

    added listener for submit

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-function initListeners() {
+function navListeners() {
   $(".bars").click(function (e) {
     $(".bars").toggleClass("active");
     $(".links").toggleClass("active");
@@ -10,6 +10,47 @@ function initListeners() {
   });
 }
 
+function createListener() {
+  $("#submit").click(function (e) {
+    // e.preventDefault();
+    let workoutImg = $("#img").val();
+    let workoutName = $("#name").val();
+    let workoutDesc = $("#description").val();
+    let workoutTime = $("#time").val();
+    let workoutCal = $("#cal").val();
+    let workoutSetOne = $("#setOne").val();
+    let workoutSetTwo = $("#setTwo").val();
+    let workoutSetThree = $("#setThree").val();
+    let workoutInstOne = $("#instOne").val();
+    let workoutInstTwo = $("#instTwo").val();
+    let workoutInstThree = $("#instThree").val();
+    alert(
+      workoutImg +
+        " " +
+        workoutName +
+        " " +
+        workoutDesc +
+        " " +
+        workoutTime +
+        " " +
+        workoutCal +
+        " " +
+        workoutSetOne +
+        " " +
+        workoutSetTwo +
+        " " +
+        workoutSetThree +
+        " " +
+        workoutInstOne +
+        " " +
+        workoutInstTwo +
+        " " +
+        workoutInstThree
+    );
+  });
+}
+
 $(document).ready(function () {
-  initListeners();
+  navListeners();
+  createListener();
 });
